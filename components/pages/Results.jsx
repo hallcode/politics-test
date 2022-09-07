ba50003f2c8b92f9d6ffb118d0ea9ec4bccf385d
Alex Hall <alexhall93@me.com>
2022-09-07 23:02:24 +0100

    Results calculations

--- components/pages/Results.jsx
+++ components/pages/Results.jsx
@@ -1,12 +1,34 @@
-import { Button, Container, Title, Text, Card, Stack } from "@mantine/core";
+import {
+  Button,
+  Container,
+  Title,
+  Text,
+  Card,
+  Stack,
+  Group,
+  Image,
+  Modal,
+  TextInput,
+  ActionIcon,
+  CopyButton,
+} from "@mantine/core";
 import { useLocalStorage, useDocumentTitle } from "@mantine/hooks";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PageLayout from "../Page";
-import calculateResults from "../../utils/calculator";
+import calculateResults, {
+  getColour,
+  getDescription,
+} from "../../utils/calculator";
 import { useState, useEffect } from "react";
-import { IconRepeat } from "@tabler/icons";
+import {
+  IconClipboard,
+  IconClipboardCheck,
+  IconCopy,
+  IconLink,
+  IconRepeat,
+  IconShare,
+} from "@tabler/icons";
 import "./results.css";
-import ResultsGraph from "../ResultsGraph";
 
 const TODAY = new Date();
 
@@ -14,6 +36,12 @@ export default function Results() {
   useDocumentTitle("Your Results | The Politics Test");
   const navigate = useNavigate();
   const [scores, setScores] = useState([0, 0]);
+  const [testDone, setTestDone] = useState(false);
+  const [showShare, setShowShare] = useState(false);
+  const [nickname, setNickname] = useState("");
+  const [token, setToken] = useState("");
+  const [description, setDescription] = useState("");
+  const [colour, setColour] = useState("");
 
   const [answers, setAnswers] = useLocalStorage({
     key: "test-responses",
@@ -29,9 +57,32 @@ export default function Results() {
   });
 
   useEffect(() => {
+    const obj = {
+      ...scores,
+      nickname,
+      description,
+    };
+
+    let json = JSON.stringify(obj);
+    let t = btoa(json);
+    console.log(json, t);
+    setToken(t);
+  }, [nickname, scores, description]);
+
+  useEffect(() => {
+    let desc = getDescription(scores);
+    let colr = getColour(scores);
+    setDescription(desc);
+    setColour(colr);
+  }, [scores]);
+
+  useEffect(() => {
     const s = calculateResults(answers);
+    if (completedQuestions.length >= 1) {
+      setTestDone(true);
+    }
     setScores(s);
-  }, [answers]);
+  }, [answers, completedQuestions]);
 
   const reset = () => {
     setAnswers([]);
@@ -44,36 +95,126 @@ export default function Results() {
       <Title size="h3" sx={(theme) => ({ color: theme.primaryColor })}>
         Results
       </Title>
-      <Text color="dimmed">You took the test on {completedDate}</Text>
+      {testDone && (
+        <Text color="dimmed">You took the test on {completedDate}</Text>
+      )}
     </Container>
   );
 
   const Footer = () => (
-    <Container size="sm" p="md">
-      <Button uppercase fullWidth onClick={reset} leftIcon={<IconRepeat />}>
-        Take the test again
+    <Container component={Group} size="sm" p="md" position="center">
+      <Button
+        uppercase
+        onClick={reset}
+        leftIcon={<IconRepeat />}
+        variant="light"
+      >
+        Take the test {testDone && "again"}
       </Button>
+      {testDone && (
+        <Button
+          uppercase
+          leftIcon={<IconShare />}
+          onClick={() => setShowShare(true)}
+        >
+          Share
+        </Button>
+      )}
     </Container>
   );
 
   return (
-    <PageLayout headerChildren={<Header />} footerChildren={<Footer />}>
-      <Container size="sm" p="lg">
+    <>
+      <PageLayout headerChildren={<Header />} footerChildren={<Footer />}>
+        <Container size="sm" p="lg">
+          <Stack>
+            {!testDone && (
+              <Card withBorder shadow="sm">
+                <Title align="center" size="h4">
+                  You need to take the test before we can show you any results
+                </Title>
+                <Text align="center" color="dimmed">
+                  Click below to take the test now.
+                </Text>
+              </Card>
+            )}
+            {testDone && (
+              <>
+                <Title align="center" size="h4">
+                  Congratulations, you're a
+                </Title>
+                <Title size={40} align="center" color={colour}>
+                  {description}
+                </Title>
+                <Card withBorder shadow="sm">
+                  <Stack>
+                    <Image
+                      src={`http://127.0.0.1:5000/s/graph/${scores.ci}/${scores.tp}`}
+                      alt="Graph showing your politics"
+                      fit="contain"
+                      withPlaceholder
+                    />
+                  </Stack>
+                </Card>
+              </>
+            )}
+          </Stack>
+        </Container>
+      </PageLayout>
+      <Modal
+        title="Create share link..."
+        centered
+        opened={showShare}
+        onClose={() => setShowShare(false)}
+      >
         <Stack>
-          <Card withBorder shadow="sm">
-            <Title align="center" size="h4">
-              Congradulations, you're a
-            </Title>
-            <Title align="center" color="blue">
-              Tory Wanker!
-            </Title>
-          </Card>
+          <TextInput
+            label="Your name"
+            description="We don't store this, it's just used so people who look at your results know who you are."
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
 
-          <Card withBorder shadow="sm">
-            <ResultsGraph ci={scores[0]} pt={scores[1]} />
-          </Card>
+          {nickname && (
+            <TextInput
+              readOnly
+              label="Personal link"
+              description="Anyone with this link will be able to see your results."
+              value={`https://www.politicaltest.com/s/share/${token}`}
+              icon={<IconLink />}
+              onClick={(e) => e.target.select()}
+            />
+          )}
+
+          <Group>
+            <Button
+              variant="light"
+              uppercase
+              onClick={() => setShowShare(false)}
+            >
+              Cancel
+            </Button>
+            {nickname && (
+              <CopyButton
+                value={`https://www.politicaltest.com/s/share/${token}`}
+              >
+                {({ copied, copy }) => (
+                  <Button
+                    color={copied ? "green" : "teal"}
+                    uppercase
+                    onClick={copy}
+                    leftIcon={
+                      copied ? <IconClipboardCheck /> : <IconClipboard />
+                    }
+                  >
+                    Copy Link
+                  </Button>
+                )}
+              </CopyButton>
+            )}
+          </Group>
         </Stack>
-      </Container>
-    </PageLayout>
+      </Modal>
+    </>
   );
 }
--- utils/calculator.js
+++ utils/calculator.js
@@ -1,5 +1,22 @@
 import { C, I, P, T } from "./questions";
 
+const POSITIONS = [
+  [[0, 0], "True centrist", "gray"],
+  [[-1, -1], "Modern Communist", "pink"],
+  [[1, 1], "Right-wing conservative", "grape"],
+  [[-1, 1], "Traditional hard-leftist", "red"],
+  [[-1, 0], "Communist", "red"],
+  [[1, 0], "Libertarian", "cyan"],
+  [[0, 1], "Traditionalist", "blue"],
+  [[0, -1], "Centrist", "yellow"],
+  [[-0.1, 0], "Lefty centrist", "orange"],
+  [[0.1, 0], "Right of centre conservative", "blue"],
+  [[0.5, 0.5], "Conservative", "blue"],
+  [[0.5, -0.5], "Liberal conservative", "indigo"],
+  [[1, -1], "Neo-liberal conservative", "violet"],
+  [[-0.66, 0], "Socialist", "red"],
+];
+
 export default function calculateResults(responses) {
   let categories = {};
   let ci_desc = "";
@@ -15,7 +32,7 @@ export default function calculateResults(responses) {
     }
   });
 
-  let results = { C: 0, I: 0, P: 0, T: 0 };
+  let results = { [C]: 0, [I]: 0, [P]: 0, [T]: 0 };
   let ci_total = 0;
   let tp_total = 0;
   let cats = Object.keys(categories);
@@ -35,4 +52,40 @@ export default function calculateResults(responses) {
     ci: ci_percent,
     tp: tp_percent,
   };
+}
+
+function distance(a, b) {
+  return Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2);
+}
+
+export function getDescription({ ci, tp }) {
+  let pos = [ci, tp];
+
+  let positions = POSITIONS;
+
+  positions.sort((a, b) => {
+    let a_dist = distance(a[0], pos);
+    let b_dist = distance(b[0], pos);
+    return a_dist - b_dist;
+  });
+
+  console.log(positions);
+
+  return positions[0][1];
+}
+
+export function getColour({ ci, tp }) {
+  let pos = [ci, tp];
+
+  let positions = POSITIONS;
+
+  positions.sort((a, b) => {
+    let a_dist = distance(a[0], pos);
+    let b_dist = distance(b[0], pos);
+    return a_dist - b_dist;
+  });
+
+  console.log(positions);
+
+  return positions[0][2];
 }
